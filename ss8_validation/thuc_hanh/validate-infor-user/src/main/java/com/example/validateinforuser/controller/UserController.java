a99636d6a0fde836dfd088a6a0d6d54705ef8228
HoNgocAn <anhongoc1306@gmail.com>
2023-10-24 20:53:50 +0700

    nop bai

---
+++ ss8_validation/thuc_hanh/validate-infor-user/src/main/java/com/example/validateinforuser/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.example.validateinforuser.controller;
+
+import org.springframework.stereotype.Controller;
+import com.example.validateinforuser.model.User;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/user")
+    public ModelAndView showForm() {
+        return new  ModelAndView("/index", "user" , new User());
+    }
+
+    @PostMapping("/validateUser")
+    public ModelAndView checkValidation(@Validated @ModelAttribute("user") User user,
+                                        BindingResult bindingResult){
+     if(bindingResult.hasFieldErrors()){
+         return new ModelAndView("/index");
+     }
+     return new ModelAndView("/result");
+    }
+
+}
